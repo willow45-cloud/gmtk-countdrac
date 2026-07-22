58f315fdbd28b55b1b6a8ecb6b27c3ab46953345
Willow495 <wlliamrl54@gmail.com>
2026-07-22 19:51:37 +0100

    MM GB
    main menu grey box and script start

---
+++ menu_settings.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+
+
+func _on_settings_pressed() -> void:
+	pass #settings screen
+
+
+func _on_start_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()

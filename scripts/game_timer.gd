2b5b77ad50fe568184855f499ed154123a898c83
Willow495 <wlliamrl54@gmail.com>
2026-07-25 18:18:25 +0100

    Reapply "Avoidthesun"
    This reverts commit b47b516030be8a56a878f8568e1dc9ef537d5d2f.

--- scripts/game_timer.gd
+++ scripts/game_timer.gd
@@ -2,10 +2,15 @@ extends Timer
 signal counted_down(number)
 @export var _count := 5
 @onready var label: Label = $Visual
+var eot = false
+@export var _quicktime := 0.5
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	_count = randi_range(3,6)
+	wait_time = randf_range(0.4, 1.25)
+	_quicktime = randf_range(0.3, 0.6)
 	$Visual.numberdisplayed = _count
 	$Visual._visualcoutdown()
 
@@ -20,10 +25,23 @@ func _on_timeout() -> void:
 	emit_signal("counted_down", _count)
 	_count -= 1
 	$Visual.numberdisplayed = _count
+	#$"../TextureRect".texture.Gradient2D = Vector4(131, 42, 1, 255)
 	$Visual._visualcoutdown()
 	print("done")
 	
 	if _count < 1:
-		stop
+		_ontimeended()
 		$Visual._visualcountdownact()
-		print()
+		stop
+		print("stopped")
+
+func _ontimeended() -> void:
+	$"../CharacterBody2D".RB = true
+	if eot == false:
+			$"../Node2D/AnimatedSprite2D".play("Sun")
+			$"../AnimationPlayer".play("Rise")
+			
+			await get_tree().create_timer(0.5).timeout
+			$"../Node2D/Sun/CollisionShape2D".position.y = 21.984
+			
+			eot = true

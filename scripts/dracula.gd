e1cdc589d9c541acf016381db1ecd3a5bc9db3f4
Willow495 <wlliamrl54@gmail.com>
2026-07-25 13:29:57 +0100

    Avoid the sun

--- scripts/dracula.gd
+++ scripts/dracula.gd
@@ -1,5 +1,9 @@
-extends Sprite2D
-var speed = 400
+extends CharacterBody2D
+#var speed = 400
+var textureduck = load("res://artwork/icon.svg")
+var texturestand = load("res://artwork/SVG.png")
+signal Overlapped()
+#var texturedeath = load()
 
 
 func _ready() -> void:
@@ -9,9 +13,18 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("left"):
-		position += Vector2.LEFT * delta * speed
+	#if Input.is_action_pressed("left"):
+		#position += Vector2.LEFT * delta * speed
+	
+	#if Input.is_action_pressed("right"):
+		#position += Vector2.RIGHT * delta * speed
 		
-	if Input.is_action_pressed("right"):
-		position += Vector2.RIGHT * delta * speed
+	if Input.is_action_just_pressed("left_click"):
+		$Sprite2D.texture = (textureduck)
+		position.y = 185.45
+		
 #left and right fixed, added to input mapping under project setting, (a,d,left,right) :3
+
+
+func _on_sun_body_entered(body: Node2D) -> void:
+	print("overlap")

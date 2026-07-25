4137ff50c0230d820ddd0d7d3124633c5d61dc75
Willow495 <wlliamrl54@gmail.com>
2026-07-25 18:27:46 +0100

    Revert "Reapply "Avoidthesun""
    This reverts commit 6164b8316494eade50d5248ec6d7fa7d50948a4b.

--- scripts/dracula.gd
+++ scripts/dracula.gd
@@ -2,8 +2,7 @@ extends CharacterBody2D
 #var speed = 400
 var textureduck = load("res://artwork/icon.svg")
 var texturestand = load("res://artwork/SVG.png")
-@export var RB = false
-
+signal Overlapped()
 #var texturedeath = load()
 
 
@@ -20,30 +19,12 @@ func _process(delta: float) -> void:
 	#if Input.is_action_pressed("right"):
 		#position += Vector2.RIGHT * delta * speed
 		
-	if Input.is_action_just_pressed("down"):
-		if RB == true:
-			$"../GameTimer/Visual".visible = not visible
-			$"../End".visible = visible
-			$Dracula.play("Duck")
-			$CollisionShape2D.position.y = 195.45
-			$"../End".text = "Dodged"
-			
-			RB == false
-			
-	#if Input.is_action_just_released("down"):
-		#if RB == true:
-			#$Dracula.play("Fear")
-			#position.y = 157
+	if Input.is_action_just_pressed("left_click"):
+		$Sprite2D.texture = (textureduck)
+		position.y = 185.45
 		
 #left and right fixed, added to input mapping under project setting, (a,d,left,right) :3
 
 
 func _on_sun_body_entered(body: Node2D) -> void:
-	$"../End".visible = visible
-	$"../GameTimer/Visual".visible = not visible
-	$Dracula.play("Dievis")
-	$"../AnimationPlayer".play("Die")
-	await get_tree().create_timer(3).timeout
-	$"../End".text = "Awful"
-	
-	
+	print("overlap")
